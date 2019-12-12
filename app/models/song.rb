dcb0603082842738e77f9fe0a088d4bb5b7f8aa6
Alouiscious Media <info@reneesoul.com>
2019-12-12 05:13:33 -0800

    corrected validations for scope and with_options

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,13 +1,17 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true, 
-  validates :title, uniqueness: { scope: %i[:release_year artist_name]
-,  message: "Song title permitted once per artist per year" }
+  validates :title, presence: true
+  validates :title, uniqueness: { 
+    scope: %i[:release_year :artist_name], message: 'song title permitted once per artist per year' 
+  }
   
-  validates :released, inclusion: {in: [true, false] }
-  validates :artist_name, presence: true
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true, uniqueness: true
 
   with_options if: :released? do |release|
-    release.validates :release_year, numerically: { less_than_or_equal_to: Date.today.year }
     release.validates :release_year, presence: true
+    release.validates :release_year, uniqueness: true
+    release.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year 
+    }
   end
 end
