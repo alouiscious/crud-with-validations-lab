a960fe4dffec2a9c4947528b066039d6f95d1aca
Alouiscious Media <info@reneesoul.com>
2019-12-12 04:41:47 -0800

    completed  model validations with conditions

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,13 @@
-class Song < ApplicationRecord
+class Song < ActiveRecord::Base
+  validates :title, presence: true, 
+  validates :title, uniqueness: { scope: %i[:release_year artist_name]
+,  message: "Song title permitted once per artist per year" }
+  
+  validates :released, inclusion: {in: [true, false] }
+  validates :artist_name, presence: true
+
+  with_options if: :released? do |release|
+    release.validates :release_year, numerically: { less_than_or_equal_to: Date.today.year }
+    release.validates :release_year, presence: true
+  end
 end
